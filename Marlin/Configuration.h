277442fdefecb92e94748f70dc5c2c9cccbfd3dc
hounslg <graham@hounslow.us>
2024-02-05 10:45:26 +0000

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1838,7 +1838,7 @@
 
 // The size of the printable area
 #define X_BED_SIZE 235
-#define Y_BED_SIZE 235
+#define Y_BED_SIZE 223
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
