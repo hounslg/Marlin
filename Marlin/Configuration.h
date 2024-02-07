f4bf129db1ee111f4b1c2604d397969b20a7d758
hounslg <graham@hounslow.us>
2024-02-07 20:35:04 +0000

    Updated steps per mm for extruder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1246,7 +1246,7 @@
  * Override with M92 (when enabled below)
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 200, 200, 400, 698.66343 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 200, 200, 400, 687.31599 }
 
 /**
  * Enable support for M92. Disable to save at least ~530 bytes of flash.
