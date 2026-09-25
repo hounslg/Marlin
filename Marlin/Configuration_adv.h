1bc6c303263902f3629c3217ff44ac5775e006cd
hounslg <graham@hounslow.us>
2026-09-25 08:48:33 +0100

    fixed tramming

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1138,11 +1138,11 @@
  *
  * Add the G35 command to measure bed corners and help adjust screws. Requires a bed probe.
  */
-//#define ASSISTED_TRAMMING
+#define ASSISTED_TRAMMING
 #if ENABLED(ASSISTED_TRAMMING)
 
   // Define from 3 to 9 points to probe.
-  #define TRAMMING_POINT_XY { {  30, 30 }, { 203,  30 }, { 203, 203 }, { 20, 203 } }
+  #define TRAMMING_POINT_XY { {  35, 35 }, { 203,  35 }, { 203, 203 }, { 35, 203 } }
 
   // Define position names for probe points.
   #define TRAMMING_POINT_NAME_1 "Front-Left"
